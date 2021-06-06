ff85f9cea9ba5c5904b81fa1989c5d0365cf4822
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-06-06 23:15:25 +0300

    Register Screen | Design & Basic Functionalities

--- app/src/main/java/com/stathis/marvelmania/features/register/RegisterActivity.kt
+++ app/src/main/java/com/stathis/marvelmania/features/register/RegisterActivity.kt
@@ -1,8 +1,17 @@
 package com.stathis.marvelmania.features.register
 
+import android.content.Intent
+import android.content.res.ColorStateList
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.stathis.marvelmania.R
 import com.stathis.marvelmania.abstraction.MarvelActivity
+import com.stathis.marvelmania.features.dashboard.DashboardActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.email_input_field
+import kotlinx.android.synthetic.main.activity_register.pass_input_field
+import kotlinx.android.synthetic.main.activity_register.register_btn
 
 class RegisterActivity : MarvelActivity(R.layout.activity_register) {
 
@@ -13,10 +22,42 @@ class RegisterActivity : MarvelActivity(R.layout.activity_register) {
     }
 
     override fun startOperations() {
+        register_btn.setOnClickListener {
+            val email = email_input_field.text.toString()
+            val pass = pass_input_field.text.toString()
+            val passConf = pass_conf_input_field.text.toString()
 
+            viewModel.validateUserInput(email, pass, passConf)
+        }
+
+        viewModel.userCreated.observe(this, Observer {
+            when (it) {
+                true -> loginUser()
+                false -> throwRegisterErrors()
+            }
+        })
+
+        viewModel.passwordsMatch.observe(this,Observer{
+            when (it){
+                true -> Unit
+                false -> throwRegisterErrors()
+            }
+        })
+    }
+
+    private fun loginUser() {
+        startActivity(Intent(this, DashboardActivity::class.java))
+    }
+
+    private fun throwRegisterErrors() {
+        /*
+        As a user I want to be notified about the fact that my passwords might not match
+         */
+        pass_input_field.error = resources.getString(R.string.wrong_password)
+        pass_conf_input_field.error = resources.getString(R.string.wrong_password)
     }
 
     override fun stopOperations() {
-
+        viewModel.userCreated.removeObservers(this)
     }
 }
--- app/src/main/java/com/stathis/marvelmania/features/register/RegisterViewModel.kt
+++ app/src/main/java/com/stathis/marvelmania/features/register/RegisterViewModel.kt
@@ -1,7 +1,28 @@
 package com.stathis.marvelmania.features.register
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterViewModel : ViewModel() {
 
+    val auth by lazy { FirebaseAuth.getInstance() }
+    val userCreated = MutableLiveData<Boolean>()
+    val passwordsMatch = MutableLiveData<Boolean>()
+
+    fun validateUserInput(email : String, pass : String, passConf : String) {
+        when(pass.equals(passConf)){
+            true -> registerUserToFirebase(email, pass)
+            false -> passwordsMatch.value = false
+        }
+    }
+
+    private fun registerUserToFirebase(email : String, pass : String){
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+            when(it.isSuccessful){
+                true -> userCreated.value = true
+                false -> userCreated.value = false
+            }
+        }
+    }
 }
