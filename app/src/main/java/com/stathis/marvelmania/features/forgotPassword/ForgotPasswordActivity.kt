e99f73603b26eefa6e5885cf69c303e826fc28be
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-06-08 23:52:05 +0300

    Forgot Password Screen | Design & Basic Functionality

---
+++ app/src/main/java/com/stathis/marvelmania/features/forgotPassword/ForgotPasswordActivity.kt
@@ -0,0 +1,42 @@
+package com.stathis.marvelmania.features.forgotPassword
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.stathis.marvelmania.R
+import com.stathis.marvelmania.abstraction.MarvelActivity
+import com.stathis.marvelmania.features.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_forgot_password.*
+
+class ForgotPasswordActivity : MarvelActivity(R.layout.activity_forgot_password) {
+
+    private lateinit var viewModel : ForgotPasswordViewModel
+
+    override fun initLayout() {
+        viewModel = ViewModelProvider(this).get(ForgotPasswordViewModel::class.java)
+    }
+
+    override fun startOperations() {
+        forgot_pass_btn.setOnClickListener{
+            val email = forgot_pass_btn.text.toString()
+            viewModel.verifyEmail(email)
+        }
+
+        viewModel.userVerified.observe(this, Observer{
+            when (it) {
+                true -> {
+                    //do something
+                    startActivity(Intent(this, LoginActivity::class.java))
+                }
+                false -> Toast.makeText(this,"User does not exist", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun stopOperations() {
+        viewModel.userVerified.removeObservers(this)
+    }
+}
---
+++ app/src/main/java/com/stathis/marvelmania/features/forgotPassword/ForgotPasswordViewModel.kt
@@ -0,0 +1,37 @@
+package com.stathis.marvelmania.features.forgotPassword
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.stathis.marvelmania.util.TAG
+
+class ForgotPasswordViewModel : ViewModel() {
+
+    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    val userVerified = MutableLiveData<Boolean>()
+
+    fun verifyEmail(email : String) {
+        auth.fetchSignInMethodsForEmail(email)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val isNewUser: Boolean? = it.result?.signInMethods?.isEmpty()
+                    if (isNewUser!!) {
+                        Log.e(TAG, "Is New User!")
+                        userVerified.value = false
+                    } else {
+                        sendEmail(email)
+                    }
+                }
+            }
+    }
+
+    private fun sendEmail(email : String) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    userVerified.value = true
+                }
+            }
+    }
+}
